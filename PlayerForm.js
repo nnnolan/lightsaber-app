2494c776577e908013b6c8cb8d0464974a7f5914
plantydev <jack673.jm@gmail.com>
2022-01-27 08:30:42 -0600

    Player Form to add players

---
+++ PlayerForm.js
@@ -0,0 +1,27 @@
+export default function PlayerForm() {
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+  
+      const form = new FormData(event.target);
+      const formData = Object.fromEntries(form.entries());
+  
+      const res = await fetch('/api/cars', {
+        body: JSON.stringify(formData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+      });
+  
+      const result = await res.json();
+      console.log(result)
+    };
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <input name="Name" type="text"  />
+  
+        <button type="submit">Create Player</button>
+      </form>
+    );
+  }
